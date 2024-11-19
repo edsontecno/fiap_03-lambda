e61ade9bdfa23b399c8fbe52d503f16a935e9153
Edson <c1297934@interno.bb.com.br>
2024-11-19 17:41:39 -0300

    ajuste caminho lambda

--- terraform/lambda/lambda.tf
+++ terraform/lambda/lambda.tf
@@ -1,13 +1,10 @@
 # Criação da Lambda Function `authorization`
 resource "aws_lambda_function" "authorization" {
-  # filename         = "./lambda/lambda_function.zip" 
+  filename         = "./lambda/lambda_function.zip" 
   function_name    = "authorization"
   role             = data.aws_iam_role.labrole.arn
   handler          = "authorization.lambda_handler"
   runtime          = "nodejs20.x" 
-
-  source_code_hash = filebase64sha256("empty_function.js")
-  filename = "empty_function.js"
 
   environment {
     variables = {
