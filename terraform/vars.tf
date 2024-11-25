437db9e8d410c87a4740382c7ef3419ff97d5d1c
Edson <c1297934@interno.bb.com.br>
2024-11-25 16:37:07 -0300

    alterando url k8s

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,7 +11,7 @@ variable "policyArn" {
 }
 
 variable "url_load_balance" {
-  default = "abbddfc7d770549abb778b312a76504d-35142584.us-east-1.elb.amazonaws.com:3000"
+  default = "https://abbddfc7d770549abb778b312a76504d-35142584.us-east-1.elb.amazonaws.com:3000"
 }
 
 variable "lambda_function_arn" {
