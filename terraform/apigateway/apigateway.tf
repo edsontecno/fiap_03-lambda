23fe42696e3a2a1ea10fd0eccc1d0e3ae120b2ae
NewMiniBoss <gabriel.f.lazari@gmail.com>
2024-12-11 20:21:52 -0300

    TESTE

--- terraform/apigateway/apigateway.tf
+++ terraform/apigateway/apigateway.tf
@@ -137,7 +137,6 @@ resource "aws_api_gateway_integration" "public_doc_integration" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "dev"
 
   depends_on = [
     aws_api_gateway_integration.auth_proxy_integration,
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "fiap-fase03-tf"
+    bucket = "bucket-fiap-student"
     key    = "lambda/terraform.tfstate"
     region = "us-east-1"
   }
