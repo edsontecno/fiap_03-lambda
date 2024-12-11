5a37d4350cc19494eb8374ccafc773f5a131b403
NewMiniBoss <gabriel.f.lazari@gmail.com>
2024-12-11 20:25:41 -0300

    teste

--- terraform/apigateway/apigateway.tf
+++ terraform/apigateway/apigateway.tf
@@ -15,6 +15,7 @@ resource "aws_api_gateway_resource" "auth_proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.auth.id
   path_part   = "{proxy+}"
+  
 }
 
 # Criação do recurso /public
