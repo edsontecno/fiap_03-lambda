fc20ee6e66784325cbbf8279e8327d7bd83e53cb
Edson <c1297934@interno.bb.com.br>
2024-12-12 20:33:20 -0300

    ajuste url lb

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,7 +11,7 @@ variable "policyArn" {
 }
 
 variable "url_load_balance" {
-  default = "http://a67395559806b4351b830cebe07d7874-1478507779.us-east-1.elb.amazonaws.com:3000"
+  default = "https://a67395559806b4351b830cebe07d7874-1478507779.us-east-1.elb.amazonaws.com:3000"
 }
 
 variable "lambda_function_arn" {
